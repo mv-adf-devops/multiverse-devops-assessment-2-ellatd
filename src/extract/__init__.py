d9499efbfdb93ea71f25888cedd6f089ec08bd5b
ellatd <68879468+ellatd@users.noreply.github.com>
2023-12-16 14:16:42 +0000

    trying (again) to merge to main branch

--- src/extract/__init__.py
+++ src/extract/__init__.py
@@ -5,6 +5,12 @@ def read_csv(filename):
     with open(filename, 'r') as f:
         for line in f.readlines():
             rows.append(line.strip().split(','))
+
+    #TICKET 2: remove duplicates
+    non_duplicates_list = []
+    for line in rows:
+        if line[0] not in [i[0] for i in non_duplicates_list]:
+            non_duplicates_list.append(line)
    
     #TICKET 3: ignore any empty lists
     removed_empty_lists = [line for line in non_duplicates_list if line != ['', '', '', '', '', '']]
